9aa84c54fcce5ff56f5355d8011ad5083a779130
JonathanRosenboim <101969613+JonathanRosenboim@users.noreply.github.com>
2022-11-27 14:22:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF
 
 output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
 output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
+output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
